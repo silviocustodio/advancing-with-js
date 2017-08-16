d3ed4d24773a9525c17a0019d15273ae57ddd61a
silviocustodio <adm.ti.silviocustodio@gmail.com>
2017-08-16 12:39:07 -0300

    4rd commit - Add new functions (save, update and delete).

--- course/js/main.js
+++ course/js/main.js
@@ -29,7 +29,7 @@ function setList() {
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) + '</td><td>'+ list[key].amount + '</td><td>'+ formatValue(list[key].value)  + '</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default">Delete</button></td></tr>';
+        table += '<tr><td>'+ formatDesc(list[key].desc) + '</td><td>'+ list[key].amount + '</td><td>'+ formatValue(list[key].value)  + '</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>';
     }
 
     table += '</tbody>';
@@ -84,7 +84,11 @@ function setUpdate(id) {
     document.getElementById("amount").value = obj.amount;
     document.getElementById("value").value = obj.value;
     document.getElementById("btnUpdate").style.display = "inline-block"; // display just when edit is clicked
-    document.getElementById("btnAdd").style.display = "none"; // display none, because it is editing 
+    document.getElementById("btnAdd").style.display = "none"; // display none, because it is editing
+
+
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
+
 }
 
 //===================== reset form ===============================================
@@ -97,7 +101,52 @@ function resetForm() {
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block"; // return to normal state
 
+
+    document.getElementById("inputIDUpdate").innerHTML = "";
+
 }
+
+//===================== update data - form  ===============================================
+
+
+function updateData() {
+
+    var id = document.getElementById("idUpdate").value;
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value =document.getElementById("value").value;
+
+    list[id] = {"desc":desc, "amount":amount, "value":value};
+
+    resetForm();
+    setList(list);
+
+
+}
+
+//===================== delete data ===============================================
+
+function deleteData(id) {
+
+    if (confirm("Delete this item?")){
+
+        if(id === list.length - 1){
+            list.pop(); //delete the last object of list
+        }else if(id ===0){
+
+            list.shift(); //delete the first object of lst
+        }else{
+            var arrAuxIni = list.slice(0,id) //get element from 0 to one element before o id
+            var arrAuxEnd = list.slice(id + 1)//get element from a number after id to all elements until the end of this list
+            list = arrAuxIni.concat(arrAuxEnd); //create array again without element with the id typed
+        }
+
+        setList();
+    }
+
+}
+
+
 
 //===================== update the list ===============================================
 
