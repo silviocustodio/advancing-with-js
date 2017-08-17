0cd0396efc47abfa531656cf4ba3f3e1f92ed56a
silviocustodio <adm.ti.silviocustodio@gmail.com>
2017-08-16 23:55:08 -0300

    5th commit - Add new functions (Validating form, setting the total get, deleting list, saving records in local storage).

--- course/js/main.js
+++ course/js/main.js
@@ -18,8 +18,7 @@ function getTotal(list) {
         total += list [key].value * list [key].amount
 
     }
-
-    return total;
+    document.getElementById("totalValue").innerHTML = formatValue(total);
 
 }
 
@@ -29,12 +28,14 @@ function setList() {
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) + '</td><td>'+ list[key].amount + '</td><td>'+ formatValue(list[key].value)  + '</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>';
+        table += '<tr><td>'+ formatDesc(list[key].desc) + '</td><td>'+ formatAmount(list[key].amount)  + '</td><td>'+ formatValue(list[key].value)  + '</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>';
     }
 
     table += '</tbody>';
 
     document.getElementById("listTable").innerHTML = table;
+    getTotal(list);
+    saveListStorage(list);
 
 }
 
@@ -45,6 +46,18 @@ function formatDesc(desc) {
     var str = desc.toLowerCase();
     str = str.charAt(0).toUpperCase() + str.slice(1)  // bring the first character from string and change it to upper, slice concatenate with the rest of word
      return str;
+
+
+}
+
+//========================= format type of amount  =========================================
+
+
+function formatAmount(amount) {
+
+    return parseInt(amount);
+
+
 
 
 }
@@ -65,6 +78,11 @@ function formatValue(value) {
 //========================= get value of input typed by user =========================================
 
 function addData() {
+
+    if (!validation()){
+        return;
+    }
+
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
@@ -103,6 +121,7 @@ function resetForm() {
 
 
     document.getElementById("inputIDUpdate").innerHTML = "";
+    document.getElementById("errors").style.display = "none";
 
 }
 
@@ -110,6 +129,11 @@ function resetForm() {
 
 
 function updateData() {
+
+    if (!validation()){
+        return;
+    }
+
 
     var id = document.getElementById("idUpdate").value;
     var desc = document.getElementById("desc").value;
@@ -146,10 +170,95 @@ function deleteData(id) {
 
 }
 
+//===================== validation  desc, amount and value ===============================================
+
+function validation() {
+
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    var errors = "";
+
+    document.getElementById("errors").style.display = "none";
 
 
-//===================== update the list ===============================================
+    if (desc ===""){
 
-setList(list);
-console.log(getTotal(list));
+        errors += '<p> Fill out description</p>'
+
+    }
+    if (amount === ""){
+
+        errors += '<p>Fill out a quantity</p>';
+    }else if(amount != parseInt(amount)){
+
+        errors += '<p>Fill out a valid amount</p>';
+
+    }
+    if (value === ""){
+
+        errors += '<p>Fill out a value</p>';
+    }else if(value != parseFloat(value)){
+
+        errors += '<p>Fill out a valid value</p>';
+
+    }
+
+    if (errors !=""){
+
+        document.getElementById("errors").style.backgroundColor = "rgba(183, 178, 178, 0.5)";
+        document.getElementById("errors").style.color = "white";
+        document.getElementById("errors").style.padding = "10px";
+        document.getElementById("errors").style.margin = "10px";
+        document.getElementById("errors").style.borderRadius = "13px";
+
+        document.getElementById("errors").style.display = "block";
+        document.getElementById("errors").innerHTML = "<h3>Error: </h3>" + errors;
+
+
+        return 0;
+    }else {
+
+        return 1;
+    }
+
+}
+
+//===================== delete list ===============================================
+
+function deleteList() {
+
+    if (confirm("Delete list?")){
+
+        list = [];
+        setList(list);
+
+    }
+
+}
+
+//===================== save list in local storage browser ===============================================
+
+function saveListStorage(list) {
+
+    var jsonStr = JSON.stringify(list); //change  array list to string format
+    localStorage.setItem("list",jsonStr);// save list in local storage
+
+}
+
+//===================== initialization function ===============================================
+
+function initListStorage() {
+
+    var testList = localStorage.getItem("list");// check if list is saved in local storage
+    if(testList){
+        list = JSON.parse(testList); //if list is valid, get attribute to list the array, recovery list
+    }
+    setList(list);
+}
+
+//===================== initiate list ===============================================
+
+initListStorage();
+
 
